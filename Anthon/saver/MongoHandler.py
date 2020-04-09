f71d1ca53d6ac3e17352087d5b98f08bc34c51f9
Yariv <yalevy@paypal.com>
2020-04-09 15:38:03 +0300

    Saver integrated for API

--- Anthon/saver/MongoHandler.py
+++ Anthon/saver/MongoHandler.py
@@ -2,11 +2,12 @@ import pymongo
 from Anthon.saver import ResultEntry, UserEntry, DBHandler
 from furl import furl
 from Anthon.common import UnsupportedSchemeException
+import Anthon.common as Common
 
 
 BASE_DB = "anthon_db"
-USERS_DOCUMENT = "users"
-SNAPSHOTS_DOCUMENT = "snapshots"
+USERS_COL = "users"
+SNAPSHOTS_COL = "snapshots"
 MONGO_DEFAULT_PATH = "mongodb://localhost:27017"
 
 
@@ -22,8 +23,8 @@ class MongoHandler(DBHandler):
         self.client = pymongo.MongoClient(host=self.host, port=self.port)
 
         self.db            = self.client[BASE_DB]
-        self.users_doc     = self.db[USERS_DOCUMENT]
-        self.snapshots_doc = self.db[SNAPSHOTS_DOCUMENT]
+        self.users_col     = self.db[USERS_COL]
+        self.snapshots_col = self.db[SNAPSHOTS_COL]
         super().__init__()
 
     def save_user(self, user_entry: UserEntry):
@@ -31,30 +32,65 @@ class MongoHandler(DBHandler):
             print("user exists")
             return
 
-        self.users_doc.insert_one(user_entry.to_json())
+        self.users_col.insert_one(user_entry.to_json())
 
     def user_id_exists(self, user_id):
-        return True if self.users_doc.find_one({'user_id': user_id}) else False
+        return True if self.get_user(user_id) else False
 
     def save_snapshot_result(self, result_entry: ResultEntry):
         if not self.snapshot_id_exists(result_entry.snapshot_id):
             print("snapshot doesn't exist")
-            self.snapshots_doc.insert_one(result_entry.to_json())
+            self.snapshots_col.insert_one(result_entry.to_json())
             return
 
-        self.snapshots_doc.update({'snapshot_id': result_entry.snapshot_id}, {'$set' : result_entry.result_to_json()})
+        self.snapshots_col.update({'snapshot_id': result_entry.snapshot_id}, {'$set' : result_entry.result_to_json()})
 
     def snapshot_id_exists(self, snapshot_id):
-        return True if self.snapshots_doc.find_one({'snapshot_id': snapshot_id}) else False
+        return True if self.snapshots_col.find_one({'snapshot_id': snapshot_id}) else False
 
-    def get_user(self):
-        pass
+    def get_all_users(self):
+        users_json_list = []
+        for user_doc in self.users_col.find():
+            user_json = dict()
+            user_json[Common.USER_ID_FIELD]  = user_doc[Common.USER_ID_FIELD]
+            user_json[Common.USERNAME_FIELD] = user_doc[Common.USERNAME_FIELD]
 
-    def get_snapshot(self):
-        pass
+            users_json_list.append(user_json)
 
-    def get_user_snapshots(self):
-        pass
+        return users_json_list
 
-    def get_snapshot_result(self):
-        pass
+    def get_user(self, user_id):
+        user_doc = self.users_col.find_one({Common.USER_ID_FIELD: user_id})
+        if user_doc is not None:
+            user_doc.pop('_id')
+
+        return user_doc
+
+    def get_user_snapshots(self, user_id):
+        snapshots_json_list = []
+
+        for snapshot_doc in self.snapshots_col.find({Common.USER_ID_FIELD: user_id}):
+            snapshot_json = dict()
+            snapshot_json[Common.SNAPSHOT_ID_FIELD] = snapshot_doc[Common.SNAPSHOT_ID_FIELD]
+            snapshot_json[Common.TIMESTAMP_FIELD]   = snapshot_doc[Common.TIMESTAMP_FIELD]
+
+            snapshots_json_list.append(snapshot_json)
+
+        return snapshots_json_list
+
+    def get_snapshot(self, snapshot_id):
+        snapshot_doc = self.snapshots_col.find({Common.SNAPSHOT_ID_FIELD: snapshot_id})
+        if snapshot_doc is not None:
+            snapshot_doc.pop('_id')
+            snapshot_doc.pop(Common.SNAPSHOT_PATH_FIELD)
+            snapshot_doc.pop(Common.USER_ID_FIELD)
+
+        return snapshot_doc
+
+    def get_snapshot_result(self, snapshot_id, topic):
+        snapshot_doc = self.get_snapshot(snapshot_id)
+        if snapshot_doc is not None:
+            if topic in snapshot_doc:
+                return snapshot_doc[topic]
+
+        return None
