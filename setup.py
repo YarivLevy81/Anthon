b10ab1360b40dbc34b92c6efb17eb8d4370fb9d9
Yariv <yalevy@paypal.com>
2020-05-12 19:37:39 +0300

    Travis?

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'FinalProject',
-    version = '0.1.0',
+    name = 'Anthon',
+    version = '1.0.0',
     author = 'Yariv Levy',
     description = 'Final project at Advanced System Design course at TAU',
     packages = find_packages(),
-    install_requires = ['click', 'flask'],
     tests_require = ['pytest', 'pytest-cov'],
 )
